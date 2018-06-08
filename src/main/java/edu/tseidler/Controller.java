dcc08260727b15a78e212a9d155fed23213c2949
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-06-08 18:38:04 +0200

    further tests

--- src/main/java/edu/tseidler/Controller.java
+++ src/main/java/edu/tseidler/Controller.java
@@ -1,15 +1,20 @@
 package edu.tseidler;
 
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 
 public class Controller {
-    private static int i = 0;
-
+    @FXML
+    GridPane main;
     @FXML
     TextArea textArea;
     @FXML
@@ -22,6 +27,16 @@ public class Controller {
     Button multiplyButton;
     @FXML
     Label result;
+
+    private static int i = 0;
+
+    public void initialize() {
+        main.addEventHandler(KeyEvent.KEY_PRESSED, event -> onEnterPressed(event));
+    }
+
+    private void onEnterPressed(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) showText();
+    }
 
     public void showText() {
         i++;
